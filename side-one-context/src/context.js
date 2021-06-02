677485f1a691bfd7fc0dcb89ac5d416e0ce0b0ff
kousiclattala <kousiclattala112@gmail.com>
2021-06-02 14:42:37 +0530

    Side One Context

--- side-one-context/src/context.js
+++ side-one-context/src/context.js
@@ -1,5 +1,5 @@
-import React, {useContext} from 'react'
+import React, { createContext } from "react";
 
-const Context = React.createContext()
+const Context = React.createContext();
 
-export default Context
+export default Context;
