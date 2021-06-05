10f06965399a5ea3abd0cded08e309c338f24aa8
kousiclattala <kousiclattala112@gmail.com>
2021-06-05 17:34:33 +0530

    Firebase-RealTime-DB

---
+++ eleven-realtime-db/src/context/Context.js
@@ -0,0 +1,5 @@
+//TODO: Create context: ContactContext ✅
+
+import { createContext } from "react";
+
+export const ContactContext = createContext();
---
+++ eleven-realtime-db/src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: add firebase configuration and export it✅
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
+
+export const firebaseConfig = {
+  apiKey: "<ADD_YOUR_APIKEY>",
+  authDomain: "<ADD_YOUR_AUTHDOMAIN>",
+  databaseURL: "<ADD_YOUR_DB>",
+  projectId: "<ADD_YOUR_PROJECTID>",
+  storageBucket: "<ADD_YOUR_STORAGEBUCKET>",
+  messagingSenderId: "<ADD_YOUR_SENDERID>",
+  appId: "<ADD_YOUR_APPID>",
+};
